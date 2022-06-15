0ab51267353d5b0f6fc163ac467c21d1388e76b4
Vadim Dudkevich <justin_duda@ukr.net>
2022-06-15 18:16:42 +0300

    add hw1 Node.js

---
+++ README.md
@@ -0,0 +1,30 @@
+<h2> Hi there 👋, I`m Vadim!
+
+## HW1 Node.js:
+
+[Screenshot 1](https://monosnap.com/file/uNsjTyOHUXaRcH7Egoe2OfXzF8OmBA)
+
+[Screenshot 2](https://monosnap.com/file/pwonFUOC6UHoc6RjG4umIm6TFGpHMb)
+
+<br />
+<br />
+
+### Connect with me:
+
+[<img align="left" alt="JS" width="26px" src="https://www.svgrepo.com/show/271118/linkedin.svg"/>][linkedin]
+[<img align="left" alt="JS" width="26px" src="https://www.svgrepo.com/show/271106/whatsapp.svg"/>][whatsapp]
+[<img align="left" alt="JS" width="26px" src="https://www.svgrepo.com/show/271095/messenger.svg"/>][messenger]
+[<img align="left" alt="JS" width="26px" src="https://www.svgrepo.com/show/271091/telegram.svg"/>][telegram]
+[<img align="left" alt="JS" width="26px" src="https://www.svgrepo.com/show/331470/mailbox.svg"/>](mailto:v.dudkevich@hotmail.com?subject=Send%20from%20GitHub)
+
+<br />
+<br />
+<br />
+<br />
+
+[linkedin]: https://www.linkedin.com/in/vadim-dudkevich/
+[whatsapp]: https://wa.me/380936005006?text=Hello!%20This%20message%20was%20submitted%20from%20Github.%20
+[messenger]: https://www.messenger.com/t/vadim.dudkevich
+[telegram]: https://t.me/vadimdudkevich
+
+![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white) ![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white) ![NPM](https://img.shields.io/badge/NPM-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white) ![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white) ![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
---
+++ index.js
@@ -0,0 +1,83 @@
+// console.log('Vadim');
+
+// const yargs = require('yargs');
+// const { hideBin } = require('yargs/helpers');
+
+const { program } = require('commander');
+
+const contacts = require('./db');
+// console.log(__dirname);
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const result = await contacts.listContacts();
+      console.log(result);
+      break;
+
+    case 'get':
+      const contact = await contacts.getContactById(id);
+      console.log(contact);
+      break;
+
+    case 'add':
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case 'update':
+      const updateById = await contacts.updateContact(id, name, email, phone);
+      console.log(updateById);
+      break;
+
+    case 'remove':
+      const remove = await contacts.removeContact(id);
+      console.log(remove);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+      break;
+  }
+};
+
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+invokeAction(argv);
+
+// const arr = hideBin(process.argv);
+// const { argv } = yargs(arr);
+// invokeAction(argv);
+
+// invokeAction({ action: 'list' });
+// invokeAction({ action: 'get', id: '5' });
+// invokeAction({
+//   action: 'add',
+//   name: 'Vadim Dukevich',
+//   email: 'duda@mail.com',
+//   phone: '(382) 556 44 645',
+// });
+// invokeAction({
+//   action: 'update',
+//   id: 'd4a8f762-153c-4213-86e7-6a153e30ce88',
+//   name: 'Mango ',
+//   email: 'mango@gmail.com',
+//   phone: '322-22-22',
+// });
+// invokeAction({ action: 'remove', id: '45196efa-fbe2-4c7a-a3e2-8c533014eb41' });
+
+// console.log(process.argv);
+
+// const actionIndex = process.argv.indexOf('--action');
+// if (actionIndex !== -1) {
+//   const action = process.argv[actionIndex + 1];
+//   invokeAction({ action });
+// }
